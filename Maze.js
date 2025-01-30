2f6fd07c64f55615cca55aae49ef0ec610cc86fd
EthanCondon <ethancondon735@gmail.com>
2025-01-30 07:46:37 -0500

    Updated Red Checkpoint + Sound #2 start

--- Maze.js
+++ Maze.js
@@ -17,7 +17,7 @@ async function startProgram() {
   // Driver: Justin Suiter
   // Navigator: Ethan Condon
   await setSpeed(60);
-  await delay(2.6);
+  await delay(2.5);
   await setSpeed(0);
   await delay(1);
   await setMainLed({ r: 0, g: 255, b: 0 });
@@ -27,24 +27,28 @@ async function startProgram() {
   // Driver: Ethan Condon
   // Navigator: Justin Suiter
   await delay(.5);
-  await roll(90,60,1.9);
+  await roll(90,60,1.8);
   await delay(.5)
   await spin(90,1);
-  await roll(180,60,1.2);
+  await roll(180,60,1.1);
   await Sound.Animal.Monkey.play(true);
 
   // Red Checkpoint
   // LED's turn Red
   // Driver: Justin Suiter
   // Navigator: Ethan Condon
-  await spin(-180,1);
+  await roll(-140,60,1.4);
+  await setMainLed({ r: 255, g: 0, b: 0 });
+  await setHeading(0, true);
 
 
   // Sound #2 Checkpoint
   // Driver: Ethan Condon
   // Navigator: Justin Suiter
-
-
+	await (.5);
+	await roll(-230,60,.8);
+	await delay(.5);
+	await roll(-320,60,.8);
   // Purple Checkpoint
   // LED's turn Purple
   // Driver: Justin Suiter
